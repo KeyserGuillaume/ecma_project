296638acfed4971062a8f1b0f9dae2155bf9bc93
KeyserGuillaume <guillaume-dekeyser@hotmail.fr>
2020-01-29 19:28:24 +0100

    modification of heuristic

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -12,7 +12,8 @@ void solve_problem_with_heuristic(IloEnv &env, const Instance &I) {
       }
   }
 
-  obj += var.z;
+    // modify MA's heuristic
+//  obj += var.z;
   master_model.add(IloMinimize(env, obj));
   obj.end();
 
@@ -25,14 +26,15 @@ void solve_problem_with_heuristic(IloEnv &env, const Instance &I) {
     expr.end();
   }
 
-  IloExpr expr (env);
-  for (unsigned int i = 0; i < I.n; i++) {
-    for (unsigned int j = i + 1; j < I.n; j++) {
-      expr += 3 * (I.lh[i] + I.lh[j]) * var.y[i][j];
-    }
-  }
-  master_model.add(var.z >= expr);
-  expr.end();
+    // modify MA's heuristic
+//  IloExpr expr (env);
+//  for (unsigned int i = 0; i < I.n; i++) {
+//    for (unsigned int j = i + 1; j < I.n; j++) {
+//      expr += 3 * (I.lh[i] + I.lh[j]) * var.y[i][j];
+//    }
+//  }
+//  master_model.add(var.z >= expr);
+//  expr.end();
 
   for (unsigned int i = 0; i < I.n; i++)
       for (unsigned int j = i + 1; j < I.n; j++)
@@ -50,6 +52,32 @@ void solve_problem_with_heuristic(IloEnv &env, const Instance &I) {
   IloCplex master_cplex(master_model);
   master_cplex.setOut(env.getNullStream()); // tell cplex to be silent
   master_cplex.solve();
-  std::cout << "Solved objective: " << master_cplex.getObjValue() << std::endl;
+
+  // modify MA's heuristic
+  //    std::cout << "Solved objective: " << master_cplex.getObjValue() << std::endl;
+  // solve slave pb 1 and compute real solution value
+    IloArray<IloNumArray> y(env, I.n);
+    for (unsigned int i=0; i<I.n; i++) {
+        y[i] = IloNumArray(env, I.n);
+    }
+    Slave1Variables slave1_var(env, I);
+    for (unsigned int i=0; i<I.n; i++) {
+        for (unsigned int j=i+1; j<I.n; j++) {
+            y[i][j] = master_cplex.getValue(var.y[i][j]);
+        }
+    }
+    IloNum z = 0;
+    bool enriching1;
+    IloArray<IloNumArray> delta1 = solve_slave_problem_1(env, I, slave1_var, y, z, enriching1);
+    double real_objective = master_cplex.getObjValue();
+    if (enriching1) {
+        for (unsigned int i = 0; i < I.n; i++) {
+            for (unsigned int j = i + 1; j < I.n; j++) {
+                real_objective += delta1[i][j] * (I.lh[i] + I.lh[j]) * y[i][j];
+            }
+        }
+    }
+
+    std::cout << "Solved objective: " << real_objective << std::endl;
 
 }
